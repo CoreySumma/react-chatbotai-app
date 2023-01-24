af3e1bae9254b0246b07d54704517ff9fc78256d
CoreySumma <CoreySumma@gmail.com>
2023-01-24 17:48:44 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+     <h1>Les Do it</h1>
+    </div>
+  )
+}
+
+export default App
