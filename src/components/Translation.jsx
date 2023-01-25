44e19adeba10bf84759b3a4be75f28226ceb8180
CoreySumma <CoreySumma@gmail.com>
2023-01-25 15:20:47 -0600

    Working app

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-export default function Translation() {
+export default function Translation({ doStuff, setInput, result }) {
   return (
-    <h1>Translate</h1>
+    <>
+    <div className="center">
+      <h1>Response:</h1>
+      <textarea 
+        className="text-area"
+        cols={55} 
+        rows={20} 
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <button className="action-btn" onClick={doStuff}>Do Your Thing</button>
+
+      <h3 className="result-text">{result.length > 0 ? result : ""}</h3>
+    </div>
+    </>
   )
 }
